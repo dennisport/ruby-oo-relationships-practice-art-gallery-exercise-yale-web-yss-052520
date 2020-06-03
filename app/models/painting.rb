4a4f53d073301df2a8bcfd5edd64eef36f190f4a
Dennis <Dennis.portillo@yale.edu>
2020-06-02 17:16:23 -0700

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,22 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :artist, :gallery, :title, :price
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(artist, gallery, title, price)
+    @artist = artist
+    @gallery = gallery
     @title = title
     @price = price
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.reduce(0) {|sum, number| sum + number.price}
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,25 @@
 require_relative '../config/environment.rb'
+require 'pry'
 
+a1 = Artist.new("Dennis", 3)
+a2 = Artist.new("A", 4)
+a3 = Artist.new("Aud", 12)
+a4 = Artist.new("Ruben", 12)
+a5 = Artist.new("Raphael", 20)
 
+g1 = Gallery.new("MOMA", "NY")
+g2 = Gallery.new("Louvre", "Paris")
+g3 = Gallery.new("MET", "NY")
+g4 = Gallery.new("Guggenheim", "Bilbao")
+g5 = Gallery.new("MOCA", "LA")
+
+p1 = Painting.new(a1, g5, "Art", 200)
+p2 = Painting.new(a1, g2, "Art2", 100)
+p3 = Painting.new(a4, g4, "Color", 200000)
+p4 = Painting.new(a5, g5, "Famous", 500000000)
+p5 = Painting.new(a5, g2, "Art", 200)
 
 
 binding.pry
-
+0
 puts "Bob Ross rules."
